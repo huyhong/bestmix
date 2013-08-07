5a100823a1ab17d7351f9027a98953591329f4d8
Quang Pham <me@quangpham.com>
2013-08-07 21:31:35 +0300

    config ios

---
+++ ios/Bestmix/Config.h
@@ -0,0 +1,6 @@
+#define WebApiUrl    @"http://localhost:3000/api/v1/"
+
+#define AuthBaseURL  @"http://localhost:3000/"
+#define ClientID     @"9880577d2c973c4f32bc8910ae5ddc44c4705d2ab7586c3ecd73cb7dee16be8d"
+#define ClientSecret @"8b67643aff821d9dd1768092cf812e2dc34631848211a16b54f6f67e294f9a3d"
+#define RedirectURL  @"bestmix://auth"
